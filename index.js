18113c4fa5e2584efc2023f3e6aa68d33a84c2c6
wpdlzm <jake01218@naver.com>
2020-11-10 21:36:32 +0900

    add 수정20

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ client.on('message', (message) => {
   if(message.content == '!help') {
     let helpImg = 'https://cdn.discordapp.com/attachments/775519421476765748/775656213228290068/1__6_-removebg-preview.png';
     let commandList = [
-      {name: '!certified', desc: '인증'},
-      {name: '!hwid id pw', desc: '24시간마다 hwid를 재설정'},
+      {name: '!인증', desc: '인증'},
+      {name: '!hwid id pw', desc: 'hwid을 재설정'},
     ];
     let commandStr = '';
     let embed = new Discord.RichEmbed()
@@ -36,44 +36,17 @@ client.on('message', (message) => {
 
     message.channel.send(embed)
    
-  } else if(message.content == '!certified') {
+  } else if(message.content == '!인증') {
     let Img = 'https://cdn.discordapp.com/attachments/775696488785117216/775696593897914368/411823403469635594.png';
     if(message.channel.type == 'dm') {
       return message.reply(Img);
-    }
   
-      
-      var clearLine = message.content.slice('!청소'.length);
-    var isNum = !isNaN(clearLine)
-
-    if(isNum && (clearLine <= 0 || 100 < clearLine)) {
-      message.channel.send("1부터 100까지의 숫자만 입력해주세요.")
-      return;
-    } else if(!isNum) { 
-      if(message.content.split('<@').length == 2) {
-        if(isNaN(message.content.split(' ')[2])) return;
-
-        var user = message.content.split(' ')[1].split('<@!')[1].split('>')[0];
-        var count = parseInt(message.content.split(' ')[2])+1;
-        let _cnt = 0;
-
-        message.channel.fetchMessages().then(collected => {
-          collected.every(msg => {
-            if(msg.author.id == user) {
-              msg.delete();
-              ++_cnt;
-            }
-            return !(_cnt == count);
-          });
-        });
-      }
-    } else {
-      message.channel.bulkDelete(parseInt(clearLine)+1)
-        .then(() => {
-          AutoMsgDelete(message, `<@${message.author.id}> ` + parseInt(clearLine) + "개의 메시지를 삭제했습니다.");
-        })
-        .catch(console.error)
-    }
+    } else if(message.content == '!invite') {
+      let invite = 'https://discord.gg/Guep5nmMaT';
+      if(message.channel.type == 'dm') {
+        return message.reply(invite);
+  }
+  } 
   }
 });
 
